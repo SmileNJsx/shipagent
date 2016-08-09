4c3e866e6a84871fd16c78b55f5a28d05cfe7026
Lunatic_Genius <731314806@qq.com>
2016-08-09 22:07:35 +0800

    ADD shipinfoform

--- src/shipagent/shipinfo/AddshipinfoServlet.java
+++ src/shipagent/shipinfo/AddshipinfoServlet.java
@@ -1,13 +1,52 @@
 package shipagent.shipinfo;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import shipagent.db.DbConnector;
+
 @SuppressWarnings("serial")
 public class AddshipinfoServlet extends HttpServlet{
+	private String EnglishshipName = null;
+	private String ChineseshipName = null;
+	private String Add_user = null;
 	@Override
-	protected void doPost(HttpServletRequest req,HttpServletResponse resp){
-		System.out.println("hello");
+	protected void doPost(HttpServletRequest req,HttpServletResponse resp) throws IOException{
+		EnglishshipName = new String(req.getParameter("EnglishshipName").getBytes("iso-8859-1"),"utf-8");
+		ChineseshipName = new String(req.getParameter("ChineseshipName").getBytes("iso-8859-1"),"utf-8");
+		Add_user = new String(req.getParameter("Add_user").getBytes("iso-8859-1"),"utf-8");
+		
+		try {
+			if(insert()){
+				resp.getWriter().println("success!");
+				resp.sendRedirect("/shipagent/shipinfoform/shipinfo.html");
+			}
+			else{
+				resp.getWriter().println("error!");
+				resp.sendRedirect("/shipagent/shipinfoform/shipinfo.html");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public boolean insert() throws SQLException{
+		String sql = "insert into t_ship_info(EnglishName,ChineseName,Add_user) values("+"'"+EnglishshipName+"'"+","+"'"+ChineseshipName+"'"+","+"'"+Add_user+"')";
+		
+		DbConnector conn = new DbConnector();
+		int num = conn.insert(sql);
+		
+		if(num>=1){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 }
