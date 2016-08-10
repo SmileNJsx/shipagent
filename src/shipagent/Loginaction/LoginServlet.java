a3282fd6e99c1f9c1745f27b3c454b28a4ac26c8
Lunatic_Genius <731314806@qq.com>
2016-08-10 21:21:14 +0800

    root

--- src/shipagent/Loginaction/LoginServlet.java
+++ src/shipagent/Loginaction/LoginServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,19 +20,25 @@ public class LoginServlet extends HttpServlet{
 	private String server_password = null;
 	
 	@Override
-	protected void doPost(HttpServletRequest req,HttpServletResponse resp) throws UnsupportedEncodingException, IOException{
+	protected void doPost(HttpServletRequest req,HttpServletResponse resp) throws UnsupportedEncodingException, IOException, ServletException{
+		
 		client_username = new String(req.getParameter("username").getBytes("iso-8859-1"),"utf-8");
 		client_password = new String(req.getParameter("password").getBytes("iso-8859-1"),"utf-8");
 		
 		try {
-			resp.sendRedirect(verification());;
+			if(verification()){
+				req.getRequestDispatcher("/WEB-INF/shipinfoform/shipinfo.html").forward(req,resp);
+			}
+			else{
+				resp.sendRedirect("/shipagent/index.html");
+			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	public String verification() throws SQLException{
+	public boolean verification() throws SQLException{
 		
 		String sql = "select password from t_users where username="+"\""+client_username+"\"";
 		
@@ -40,14 +47,14 @@ public class LoginServlet extends HttpServlet{
 		server_password = conn.select(sql, "password");
 		
 		if(server_password == null){
-			return "/shipagent/index.html";
+			return false;
 		}
 		
 		if(client_password.equals(server_password)){
-			return "/shipagent/shipinfoform/shipinfo.html";
+			return true;
 		}
 		else{
-			return "/shipagent/index.html";
+			return false;
 		}
 	}
 }
